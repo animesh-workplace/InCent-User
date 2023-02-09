1651a75a838f6cd6fb70d8707980e9518fe0929a
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-02-09 10:09:48 +0530

    updated file - components/Form/CompanyDetail.vue

---
+++ components/Form/CompanyDetail.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="d-flex">
+		<TableGeneral />
+		<TableFileUpload />
+	</div>
+</template>
+
+<script>
+export default {
+	data: () => ({}),
+	components: {},
+	methods: {},
+	mounted() {
+		this.$nextTick(() => {})
+	},
+}
+</script>
+
+<style></style>
