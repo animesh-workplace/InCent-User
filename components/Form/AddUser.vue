7132afbff4f1bb63c0092a982e495c171d816d63
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-02-09 10:09:48 +0530

    updated file - components/Form/AddUser.vue

---
+++ components/Form/AddUser.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="white">
+		<div class="d-flex justify-space-between align-center py-6 px-10">
+			<h5 class="header-text text-h6">Add User</h5>
+			<h6 class="subtitle-text text-subtitle">
+				( Following user data will be reflected on the screen once filled )
+			</h6>
+		</div>
+		<v-divider color="#D4D5D8"></v-divider>
+
+		<div class="d-flex justify-center align-center flex-column pa-4">
+			Lorem ipsum dolor sit, amet consectetur, adipisicing elit. Natus dicta, aspernatur culpa error soluta
+			aliquam impedit, animi libero vel architecto perspiciatis, laborum. Cum fuga eum ipsam mollitia minima
+			ipsum quidem.
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	data: () => ({}),
+	components: {},
+	methods: {},
+	mounted() {
+		this.$nextTick(() => {})
+	},
+}
+</script>
+
+<style></style>
