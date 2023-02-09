abdf4dad3a6f8df74aa25c98760a4717b74de845
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-02-09 10:09:51 +0530

    updated file - layouts/default.vue

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<Nuxt />
+	</div>
+</template>
+
+<script>
+export default {
+	data: () => ({}),
+	components: {},
+	methods: {},
+	mounted() {
+		this.$nextTick(() => {})
+	},
+}
+</script>
+
+<style></style>
