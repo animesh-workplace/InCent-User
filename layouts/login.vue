8b952ca0b0e5c9e7ecef8856b0f72f717463174a
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-02-09 10:09:51 +0530

    updated file - layouts/login.vue

---
+++ layouts/login.vue
@@ -0,0 +1,28 @@
+<template>
+	<v-app>
+		<CommonHeader login />
+		<Nuxt />
+		<CommonFooter />
+	</v-app>
+</template>
+
+<script>
+// import * as login from '@/utils/api/LoginAPI'
+
+export default {
+	data: () => ({
+		drawer: true,
+	}),
+	components: {},
+	methods: {},
+	mounted() {
+		this.$nextTick(() => {
+			// if (login.default.isUserLoggedIn()) {
+			// 	this.$router.push('/admin')
+			// }
+		})
+	},
+}
+</script>
+
+<style></style>
