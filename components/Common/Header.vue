f4e36446a3c504b733f6d33b97b0db153e0e9cb2
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-02-09 10:09:47 +0530

    updated file - components/Common/Header.vue

---
+++ components/Common/Header.vue
@@ -0,0 +1,21 @@
+<template>
+	<v-app-bar clipped-left fixed app color="white" :height="login ? 90 : undefined">
+		<img src="@/assets/logo.png" alt="Logo" :height="login ? '80px' : '60px'" />
+		<v-spacer v-if="!login" />
+		<v-btn outlined rounded color="design" class="px-6" v-if="!login">Login</v-btn>
+	</v-app-bar>
+</template>
+
+<script>
+export default {
+	data: () => ({}),
+	props: { login: { default: false, type: Boolean } },
+	components: {},
+	methods: {},
+	mounted() {
+		this.$nextTick(() => {})
+	},
+}
+</script>
+
+<style></style>
