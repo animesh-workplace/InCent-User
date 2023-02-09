054799853701d64e3d9f0d2071e6f4b6a8968fe5
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-02-09 10:09:53 +0530

    updated file - pages/index.vue

---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+	<v-sheet class="margin-high mx-auto rounded-lg center" elevation="4">
+		<v-row>
+			<v-col align-self="center">
+				<img src="@/assets/hero.png" alt="Hero" />
+			</v-col>
+			<v-col class="pa-4 border-col">
+				<FormDefaultLogin @OTPLogin="ShowOTP" v-if="!show_otp" />
+				<FormOTPLogin v-else />
+			</v-col>
+		</v-row>
+	</v-sheet>
+</template>
+
+<script>
+export default {
+	data: () => ({
+		show_otp: false,
+	}),
+	layout: 'login',
+	components: {},
+	methods: {
+		ShowOTP() {
+			this.show_otp = !this.show_otp
+		},
+	},
+	mounted() {
+		this.$nextTick(() => {})
+	},
+}
+</script>
+
+<style scoped>
+.margin-high {
+	margin-top: 100px;
+}
+.center {
+	width: 70%;
+	margin: auto;
+	padding: 10px;
+}
+.border-col {
+	border-left: 2px solid #c4c4c4;
+}
+</style>
