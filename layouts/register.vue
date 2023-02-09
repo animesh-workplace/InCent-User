2c5ef36e7096d2acc839ca3f34bbf0be2aad9c0f
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-02-09 10:09:51 +0530

    updated file - layouts/register.vue

---
+++ layouts/register.vue
@@ -0,0 +1,30 @@
+<template>
+	<v-app>
+		<CommonHeader />
+		<div class="py-8 px-16">
+			<Nuxt />
+		</div>
+		<CommonFooter />
+	</v-app>
+</template>
+
+<script>
+// import * as login from '@/utils/api/LoginAPI'
+
+export default {
+	data: () => ({
+		drawer: true,
+	}),
+	components: {},
+	methods: {},
+	mounted() {
+		this.$nextTick(() => {
+			// if (login.default.isUserLoggedIn()) {
+			// 	this.$router.push('/admin')
+			// }
+		})
+	},
+}
+</script>
+
+<style></style>
