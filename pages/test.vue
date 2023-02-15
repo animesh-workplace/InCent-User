98d15954d84b148bdb33e742603e1dcd4deec08e
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-02-15 17:11:47 +0530

    updated file - pages/test.vue

---
+++ pages/test.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="px-15">
+		<v-file-input :hide-input="xyz" prepend-icon="" ref="file">
+			<template v-slot:append-outer>
+				<v-btn v-if="xyz" outlined rounded color="blue" class="px-6 mb-6" @click="ClickOn">Upload</v-btn>
+			</template>
+			<template v-slot:selection="{ text }">
+				<div>selected state {text}</div>
+			</template>
+		</v-file-input>
+	</div>
+</template>
+
+<script>
+export default {
+	data: () => ({
+		xyz: true,
+	}),
+	layout: 'admin',
+	components: {},
+	methods: {
+		ClickOn() {
+			console.log('Clickedddddd')
+			this.$refs.file.$el.querySelector('input').click()
+			this.xyz = false
+		},
+	},
+	mounted() {
+		this.$nextTick(() => {
+			this.$store.dispatch('UpdateSideBar', 'Add Program Inventory')
+		})
+	},
+}
+</script>
+
+<style></style>
