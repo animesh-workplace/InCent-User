726568b20e6dbe167d3635cc6bdac7bf3a8a9bf7
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-02-15 17:11:47 +0530

    updated file - pages/sales.vue

---
+++ pages/sales.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="px-15"></div>
+</template>
+
+<script>
+export default {
+	data: () => ({
+		xyz: true,
+	}),
+	layout: 'admin',
+	components: {},
+	methods: {},
+	mounted() {
+		this.$nextTick(() => {
+			this.$store.dispatch('UpdateSideBar', 'Buy Program Inventory')
+		})
+	},
+}
+</script>
+
+<style></style>
